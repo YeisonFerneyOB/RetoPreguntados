c34a5d655b75070ca013b109a957188f7e913ecf
YeisonFerneyOB <107292762+YeisonFerneyOB@users.noreply.github.com>
2022-08-21 20:12:22 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,45 +1,60 @@
+//variables de las preguntas aleatorias
 let preguntas_aleatorias = true;
+//mostrar el mddal de juego terminado 
 let mostrar_pantalla_juego_términado = true;
+//reiniciar los pubntos
 let reiniciar_puntos_al_reiniciar_el_juego = true;
 
+
+
+
+//ejecuta apenas se abre nuestra pagina
 window.onload = function () {
-  base_preguntas = readText("base-preguntas.json");
-  interprete_bp = JSON.parse(base_preguntas);
+  base_preguntas = readText("base-preguntas.json"); // todas las preguntas las leo con mi funcion sincrona
+  interprete_bp = JSON.parse(base_preguntas); // nuestra base de preguntas
   escogerPreguntaAleatoria();
 };
 
 let pregunta;
+
 let posibles_respuestas;
+// boton a cada pregunta 
 btn_correspondiente = [
   select_id("btn1"),
   select_id("btn2"),
   select_id("btn3"),
   select_id("btn4")
 ];
+//arreglo donde guardo todas mis preguntas
 let npreguntas = [];
 
 let preguntas_hechas = 0;
 let preguntas_correctas = 0;
 
+
+// elijo las preguntas de forma aleatorias con Math.floor
 function escogerPreguntaAleatoria() {
-  let n;
+ let n;
   if (preguntas_aleatorias) {
     n = Math.floor(Math.random() * interprete_bp.length);
   } else {
     n = 0;
   }
 
+  //aqui realizo la suma de las preguntas correctas
   while (npreguntas.includes(n)) {
     n++;
     if (n >= interprete_bp.length) {
       n = 0;
     }
     if (npreguntas.length == interprete_bp.length) {
-      //Aquí es donde el juego se reinicia
+      //Aquí es donde el juego se reinicia y uso la libreria
       if (mostrar_pantalla_juego_términado) {
+        //sweetalert2.all.min.js libreria usada para mostrar el modal
         swal.fire({
-          title: "Juego finalizado",
+          title: "Juego finalizado", //puntuacion en el modal con la libreria
           text:
+          //suma de las preguntas correctas menos preguntas echas
             "Puntuación: " + preguntas_correctas + "/" + (preguntas_hechas - 1),
           icon: "success"
         });
@@ -51,16 +66,22 @@ function escogerPreguntaAleatoria() {
       npreguntas = [];
     }
   }
+  //le hago push de las respuestas en el html
   npreguntas.push(n);
+//las preguntas ya hechas 
   preguntas_hechas++;
 
   escogerPregunta(n);
 }
 
+// imprimir las preguntas que estan en el Json 
 function escogerPregunta(n) {
   pregunta = interprete_bp[n];
+  //mostramos la pregunta categoria
   select_id("categoria").innerHTML = pregunta.categoria;
+  //mostrar la pregunta pregunta
   select_id("pregunta").innerHTML = pregunta.pregunta;
+  //index de cada pregunta que aparece al lado derecho del html
   select_id("numero").innerHTML = n;
   let pc = preguntas_correctas;
   if (preguntas_hechas > 1) {
@@ -68,7 +89,7 @@ function escogerPregunta(n) {
   } else {
     select_id("puntaje").innerHTML = "";
   }
-
+  // objec fit para darle estilo a la imagen la
   style("imagen").objectFit = pregunta.objectFit;
   desordenarRespuestas(pregunta);
   if (pregunta.imagen) {
@@ -83,16 +104,20 @@ function escogerPregunta(n) {
     }, 500);
   }
 }
-
+// elijo mi respuesta , donde inserto el texto en los botones
 function desordenarRespuestas(pregunta) {
-  posibles_respuestas = [
+  posibles_respuestas = [ // arreglo con las respuestas le asigno su valor
+
     pregunta.respuesta,
     pregunta.incorrecta1,
     pregunta.incorrecta2,
     pregunta.incorrecta3,
   ];
+  // desordeno el arreglo con funcion sort cn el Math.random a
   posibles_respuestas.sort(() => Math.random() - 0.5);
 
+
+  //le paso a los botones las respuestas 
   select_id("btn1").innerHTML = posibles_respuestas[0];
   select_id("btn2").innerHTML = posibles_respuestas[1];
   select_id("btn3").innerHTML = posibles_respuestas[2];
@@ -101,6 +126,10 @@ function desordenarRespuestas(pregunta) {
 
 let suspender_botones = false;
 
+
+//funcion para oprimir los botones donde cuando presiono el boton y si es el la respuesta correcta 
+//si la possible respuesta es == em la posicion i correcto y marca en verde y listo al completar le dice su puntaje es tal
+// se reinia nuevamente a blanco cada 3 segundos con un settime Up
 function oprimir_btn(i) {
   if (suspender_botones) {
     return;
@@ -124,7 +153,6 @@ function oprimir_btn(i) {
   }, 3000);
 }
 
-// let p = prompt("numero")
 
 function reiniciar() {
   for (const btn of btn_correspondiente) {
@@ -133,14 +161,19 @@ function reiniciar() {
   escogerPreguntaAleatoria();
 }
 
+// seleciono el id de cada objeto segun el query
 function select_id(id) {
   return document.getElementById(id);
 }
-
+//selecciono el objeto de stylo
 function style(id) {
   return select_id(id).style;
 }
 
+
+
+
+//leo la base de datos de preguntas con una funcion sincrona
 function readText(ruta_local) {
   var texto = null;
   var xmlhttp = new XMLHttpRequest();
